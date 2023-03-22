37eb3ac5da427e9d8c1af0f495d4a538518dbf03
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-03-22 19:11:28 +0000

    decorator

--- src/main/java/ma/usmba/Main.java
+++ src/main/java/ma/usmba/Main.java
@@ -1,6 +1,7 @@
 package ma.usmba;
 
 import ma.usmba.composants.Boisson;
+import ma.usmba.composants.Espresso;
 import ma.usmba.composants.Sumatra;
 import ma.usmba.decorateurs.Caramel;
 import ma.usmba.decorateurs.Chocolat;
@@ -28,5 +29,11 @@ public class Main {
         boisson=new Noisette(boisson);
         System.out.println(boisson.getDescription());
         System.out.println(boisson.cout());
+
+        Boisson b=new Chocolat(new Caramel(new Noisette(new Espresso())));
+        System.out.println(b.getDescription());
+        System.out.println(b.cout());
+
+
     }
 }
