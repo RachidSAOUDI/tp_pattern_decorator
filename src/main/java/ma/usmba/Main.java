2ba4e2cb72e49af313b833b0e54dd5dc5afd88e8
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-03-22 18:40:02 +0000

    decorator

--- src/main/java/ma/usmba/Main.java
+++ src/main/java/ma/usmba/Main.java
@@ -1,9 +1,10 @@
 package ma.usmba;
 
 import ma.usmba.composants.Boisson;
-import ma.usmba.composants.Espresso;
 import ma.usmba.composants.Sumatra;
+import ma.usmba.decorateurs.Caramel;
 import ma.usmba.decorateurs.Chocolat;
+import ma.usmba.decorateurs.Noisette;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,6 +14,18 @@ public class Main {
         System.out.println(boisson.cout());
         System.out.println("-----------------------------");
         boisson=new Chocolat(boisson);
+        System.out.println(boisson.getDescription());
+        System.out.println(boisson.cout());
+        System.out.println("-----------------------------");
+        boisson=new Caramel(boisson);
+        System.out.println(boisson.getDescription());
+        System.out.println(boisson.cout());
+        System.out.println("-----------------------------");
+        boisson=new Chocolat(boisson);
+        System.out.println(boisson.getDescription());
+        System.out.println(boisson.cout());
+        System.out.println("-----------------------------");
+        boisson=new Noisette(boisson);
         System.out.println(boisson.getDescription());
         System.out.println(boisson.cout());
     }
---
+++ src/main/java/ma/usmba/decorateurs/Caramel.java
@@ -0,0 +1,20 @@
+package ma.usmba.decorateurs;
+
+import ma.usmba.composants.Boisson;
+
+public class Caramel extends Decorateur {
+
+    public Caramel(Boisson boisson) {
+        super(boisson);
+    }
+
+    @Override
+    public String getDescription() {
+        return boisson.getDescription()+" Au Caramel";
+    }
+
+    @Override
+    public double cout() {
+        return 0.8+boisson.cout();
+    }
+}
